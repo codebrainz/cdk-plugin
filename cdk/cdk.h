fc1dce67f844e124999e23667c33ae7c957cba69
Matthew Brush <mbrush@codebrainz.ca>
2015-07-27 19:32:59 -0700

    Add cdk.h to VCS
    This file used to be generated by Vala but now it's a manually
    written file used for consumers of the helper library to include
    all headers at once.

---
+++ cdk/cdk.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2015, Matthew Brush <mbrush@codebrainz.ca>
+ * All rights reserved. See the COPYING file for full license.
+ */
+
+#ifndef CDK_H_
+#define CDK_H_
+
+#include <cdk/cdkcompleter.h>
+#include <cdk/cdkdiagnostics.h>
+#include <cdk/cdkdocumenthelper.h>
+#include <cdk/cdkhighlighter.h>
+#include <cdk/cdkplugin.h>
+#include <cdk/cdkstyle.h>
+#include <cdk/cdkstylescheme.h>
+#include <cdk/cdkutils.h>
+
+#include <geanyplugin.h>
+#include <clang-c/Index.h>
+
+#endif // CDK_H_
